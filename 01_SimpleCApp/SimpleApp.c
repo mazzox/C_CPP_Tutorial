3cccdd2bc84ab082ff96af3e15ff10be93bde6e7
Moaliz.NC <mohammadalizamani@hotmail.com>
2023-01-15 03:05:29 +0330

    - Modified code with simple math

--- 01_SimpleCApp/SimpleApp.c
+++ 01_SimpleCApp/SimpleApp.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
-    printf("Hello");
-    printf(" World!");
+    int32_t x = 5;
+
+    int32_t y = x * x + 5 * x + 5;
+    
+
+    printf("f(%i) = %i", x, y);
     
 }
